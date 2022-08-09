39fb71d8a50e54a65eaaa8f9dcb3a000bde57a1b
Guuzzeji <5lh8dl440f@gmail.com>
2022-08-09 13:20:40 -0700

    [Fix] File path bug
    Fix file path bug (wouldn't run b/c ffmpeg/tmp could not be created)

--- ffmpeg/checkFiles.js
+++ ffmpeg/checkFiles.js
@@ -3,15 +3,15 @@ const path = require('path');
 
 //! Remove work files and checks/creates work folder if needed
 module.exports.checkFiles = function () {
-    if (fs.existsSync(path.resolve("./ffmpeg/tmp/output.mp4"))) {
-        fs.unlinkSync(path.resolve("./ffmpeg/tmp/output.mp4"));
+    if (fs.existsSync(path.resolve("./yt-scissors/ffmpeg/tmp/output.mp4"))) {
+        fs.unlinkSync(path.resolve("./ffmpeg/tmp/output.mp4"), { recursive: true });
     }
 
-    if (fs.existsSync(path.resolve("./ffmpeg/tmp/input.mp4"))) {
-        fs.unlinkSync(path.resolve("./ffmpeg/tmp/input.mp4"));
+    if (fs.existsSync(path.resolve("/yt-scissors/ffmpeg/tmp/input.mp4"))) {
+        fs.unlinkSync(path.resolve("./ffmpeg/tmp/input.mp4"), { recursive: true });
     }
 
-    if (!fs.existsSync(path.resolve("./ffmpeg/tmp"))) {
-        fs.mkdirSync(path.resolve("./ffmpeg/tmp"));
+    if (!fs.existsSync(path.resolve("./yt-scissors/ffmpeg/tmp"))) {
+        fs.mkdirSync(path.resolve("./ffmpeg/tmp"), { recursive: true });
     }
 };
